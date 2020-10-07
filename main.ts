10cb7de6175199396393c157f4258a3eb86e3f71
Nina-Mathew <71400436+Nina-Mathew@users.noreply.github.com>
2020-10-07 12:52:20 -0400

    POTENTIOMETER

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+// Created by: Nina Mathew
+// Created on: 10/07/20
+// This program displays the current value of a potentiometer.
 basic.forever(function () {
-	
+    let potentiometer = 0
+    basic.showNumber(pins.analogReadPin(AnalogPin.P1))
+    basic.pause(1000)
+    basic.showNumber(potentiometer)
 })
